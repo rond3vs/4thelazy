786bd380dd355b70aec185edd7188c9b0145e72e
a5design-co <laron@a5design.co>
2025-03-26 13:19:51 -0500

    Fix base setting and rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   site: 'https://rond3vs.github.io',
-  base: '/4thelazy',
+  //base: '/4thelazy',
   build: {
     assets: 'assets',
-    // Ensure assets are inlined or properly referenced
-    inlineStylesheets: 'auto', // Optional: Reduces HTTP requests
+    inlineStylesheets: 'auto',
   },
-  // Add this if you use markdown files with front matter
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: { theme: 'github-dark' }
