eb3f01bedf2ac77b35ceb7d96e999085824f6d76
rond3vs <rond3vs@gmail.com>
2025-03-26 15:07:51 -0500

    Fix asset paths by updating Astro base URL configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   site: 'https://rond3vs.github.io',
-  //base: '/4thelazy',
+  base: '/4thelazy',
   build: {
     assets: 'assets',
     inlineStylesheets: 'auto',
